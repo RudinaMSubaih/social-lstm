7d9ef2171a00209b60c9b116bee1a10f94331d35
Rudina <r.subaih@fz-juelich.de>
2021-01-19 20:04:27 +0100

    draw trajectories

---
+++ plot_traj.py
@@ -0,0 +1,37 @@
+from numpy import *
+import numpy as np
+import os, sys, glob, math
+import argparse
+import matplotlib.pyplot as plt
+
+
+def getParserArgs():
+    parser = argparse.ArgumentParser(description='Plot trajectory')
+    parser.add_argument("-p", "--path", default="./", help='give the path of source file')
+    parser.add_argument("-f", "--fps", default="25", type=float, help='give the frame rate of data')
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = getParserArgs()
+    path = args.path
+    print(path)
+    fps = args.fps
+    figname = "rho_v_%s_traj" % (os.path.basename(os.path.splitext(path)[0]))
+
+    plt.rc('xtick', labelsize=25)
+    plt.rc('ytick', labelsize=25)
+    plt.rc("font", size=30)
+    plt.rc('pdf', fonttype=42)
+    fig = plt.figure(figsize=(16, 16), dpi=100)
+
+
+    data=loadtxt(path,usecols = (0,1,2,3))
+    plt.plot(data[:,2],data[:,3])
+    #plt.axvline(x=-2.5,linestyle="-",linewidth=1,color="r")
+
+
+    plt.xlabel("x [$m$]", size=25)
+    plt.ylabel("y [$m$]", size=25)
+    plt.savefig("%s/%s" % ("./", figname))
+    plt.close()
